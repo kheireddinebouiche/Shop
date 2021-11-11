f70e80b2cdaca7f4c658c9b5e7952d4c48b2ecda
kheireddinebouiche <kheirddinebouiche@gmail.com>
2021-11-11 14:48:37 +0100

    Ajouter une autorisation sur le hote dans settings.py

--- shop/settings.py
+++ shop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'kztl!hewss%%wql#5vv!=8cnush#sz8%*^b6$7vs*_yz(di51r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1","saldaeshop.pythonanywhere.com"]
 
 
 # Application definition
